1aa133c31d3ea1c5b0ef536ebd728888bccbe027
kovian <fevamko@gmail.com>
2018-02-13 14:47:44 +0100

    Refactoring of JMS communications

--- service/src/main/java/eu/europa/ec/fisheries/uvms/user/service/bean/UserServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/user/service/bean/UserServiceBean.java
@@ -1,15 +1,15 @@
 /*
- * Developed by the European Commission - Directorate General for Maritime 
+ * Developed by the European Commission - Directorate General for Maritime
  * Affairs and Fisheries © European Union, 2015-2016.
- * 
+ *
  * This file is part of the Integrated Fisheries Data Management (IFDM) Suite.
  * The IFDM Suite is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
- * The IFDM Suite is distributed in the hope that it will be useful, but 
+ * The IFDM Suite is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details. You should have received a copy of the GNU General Public 
+ * more details. You should have received a copy of the GNU General Public
  * License along with the IFDM Suite. If not, see http://www.gnu.org/licenses/.
  */
 package eu.europa.ec.fisheries.uvms.user.service.bean;
@@ -50,120 +50,118 @@ public class UserServiceBean implements UserService {
 
     @EJB
     private OrganisationService organisationService;
-    
+
     @EJB
     private DeploymentService deploymentService;
-    
+
     /**
      * {@inheritDoc}
      *
      * @param userContextId
      * @throws UserServiceException
      */
-    public UserContext getUserContext(UserContextId userContextId) throws UserServiceException {
+    public UserContext getUserContext(UserContextId userContextId) {
         UserContextQuery userContextQuery = UserContextConverter.convertToContextQuery(userContextId);
         return UserContextConverter.convertInformationModelToUserModel(informationService.getUserContext(userContextQuery));
     }
 
     @Override
-    public void updateUserPreferences(UserContext request) throws UserServiceException {
+    public void updateUserPreferences(UserContext request) {
         informationService.updateUserPreferences(UserContextConverter.convertUserModelToInformationModel(request));
     }
 
     @Override
-    public void deployApplication(Application application) throws UserServiceException {
+    public void deployApplication(Application application) {
         deploymentService.deployApplication(ApplicationConverter.convertUserModelToInformationModel(application));
     }
 
     @Override
-    public void redeployApplication(Application application) throws UserServiceException {
+    public void redeployApplication(Application application) {
         deploymentService.redeployApplication(ApplicationConverter.convertUserModelToInformationModel(application));
     }
 
     @Override
-    public void undeployApplication(String request) throws UserServiceException {
+    public void undeployApplication(String request) {
         deploymentService.undeployApplication(request);
-        
+
     }
 
     @Override
-    public Application getDeploymentDescriptor(String applicationName) throws UserServiceException {
-        return ApplicationConverter.convertInformationModelToUserModel(deploymentService.getDeploymentDescriptor(applicationName));                
+    public Application getDeploymentDescriptor(String applicationName) {
+        return ApplicationConverter.convertInformationModelToUserModel(deploymentService.getDeploymentDescriptor(applicationName));
     }
 
     @Override
-    public Organisation getOrganisation(String organisationName) throws UserServiceException {
+    public Organisation getOrganisation(String organisationName) {
         return OrganisationConverter.convertInformationModelToUserModel(informationService.getOrganisation(organisationName));
     }
 
     @Override
-    public List<Organisation> getAllOrganisations(GetAllOrganisationRequest request) throws UserServiceException {
-        List<eu.europa.ec.mare.usm.administration.domain.Organisation> organisationList= new ArrayList<eu.europa.ec.mare.usm.administration.domain.Organisation> ();
-            List<eu.europa.ec.mare.usm.administration.domain.Organisation> allOrganisationList= organisationService.findOrganisations(USMServiceBuilder.buildAdministrationServiceRequestForAll(request)).getResults();
-            for (eu.europa.ec.mare.usm.administration.domain.Organisation organisation :allOrganisationList ) {
-                organisationList.add( organisationService.getOrganisation( USMServiceBuilder.buildAdministrationServiceRequestForId(organisation.getOrganisationId(), request)));
-            }
+    public List<Organisation> getAllOrganisations(GetAllOrganisationRequest request) {
+        List<eu.europa.ec.mare.usm.administration.domain.Organisation> organisationList = new ArrayList<eu.europa.ec.mare.usm.administration.domain.Organisation>();
+        List<eu.europa.ec.mare.usm.administration.domain.Organisation> allOrganisationList = organisationService.findOrganisations(USMServiceBuilder.buildAdministrationServiceRequestForAll(request)).getResults();
+        for (eu.europa.ec.mare.usm.administration.domain.Organisation organisation : allOrganisationList) {
+            organisationList.add(organisationService.getOrganisation(USMServiceBuilder.buildAdministrationServiceRequestForId(organisation.getOrganisationId(), request)));
+        }
         return OrganisationConverter.convertAdministrationModelToUserModel(organisationList);
     }
 
     @Override
-    public List<Organisation> findOrganisations(String nationIsoName) throws UserServiceException {
-        return OrganisationConverter.convertInformationModelToUserModel(
-        		informationService.findOrganisations(nationIsoName));
+    public List<Organisation> findOrganisations(String nationIsoName) {
+        return OrganisationConverter.convertInformationModelToUserModel(informationService.findOrganisations(nationIsoName));
     }
 
     @Override
-    public ContactDetails getContactDetails(String userName) throws UserServiceException {
-        return ContactDetailsConverter.convertInformationModelToUserModel( informationService.getContactDetails(userName));
-        
+    public ContactDetails getContactDetails(String userName) {
+        return ContactDetailsConverter.convertInformationModelToUserModel(informationService.getContactDetails(userName));
+
     }
 
-	@Override
-	public void putPreference(UserPreference userPreference) throws UserServiceException {
-		eu.europa.ec.mare.usm.information.domain.UserPreference pref =
-			UserPreferenceConverter.convertUserModelToInformationModel(userPreference);
-		eu.europa.ec.mare.usm.information.domain.UserPreference prefRetrieved = 
-				informationService.getUserPreference(pref);
-		if (prefRetrieved.getOptionName() == null) {
-			informationService.createUserPreference(pref);
-		} else {
-			informationService.updateUserPreference(pref);
-		}
-	}
-    
     @Override
-	public void createPreference(UserPreference userPreference) throws UserServiceException {
-		informationService.createUserPreference(UserPreferenceConverter.convertUserModelToInformationModel(userPreference));
-	}
+    public void putPreference(UserPreference userPreference) {
+        eu.europa.ec.mare.usm.information.domain.UserPreference pref = UserPreferenceConverter.convertUserModelToInformationModel(userPreference);
+        eu.europa.ec.mare.usm.information.domain.UserPreference prefRetrieved = informationService.getUserPreference(pref);
+        if (prefRetrieved.getOptionName() == null) {
+            informationService.createUserPreference(pref);
+        } else {
+            informationService.updateUserPreference(pref);
+        }
+    }
 
-	@Override
-	public void deletePreference(UserPreference userPreference) throws UserServiceException {
-		informationService.deleteUserPreference(UserPreferenceConverter.convertUserModelToInformationModel(userPreference));	}
+    @Override
+    public void createPreference(UserPreference userPreference) {
+        informationService.createUserPreference(UserPreferenceConverter.convertUserModelToInformationModel(userPreference));
+    }
 
-	@Override
-	public void updatePreference(UserPreference userPreference) throws UserServiceException {
-		informationService.updateUserPreference(UserPreferenceConverter.convertUserModelToInformationModel(userPreference));
-	}
+    @Override
+    public void deletePreference(UserPreference userPreference) {
+        informationService.deleteUserPreference(UserPreferenceConverter.convertUserModelToInformationModel(userPreference));
+    }
 
-	@Override
-	public void createDataset(DatasetExtension dataset) throws UserServiceException {
-		informationService.createDataSet(DatasetConverter.convertUserModelToDomainInformationModel(dataset));
-	}
+    @Override
+    public void updatePreference(UserPreference userPreference) {
+        informationService.updateUserPreference(UserPreferenceConverter.convertUserModelToInformationModel(userPreference));
+    }
 
-	@Override
-	public void deleteDataset(DatasetExtension dataset) throws UserServiceException {
-		informationService.deleteDataSet(DatasetConverter.convertUserModelToDomainInformationModel(dataset));
-	}
+    @Override
+    public void createDataset(DatasetExtension dataset) {
+        informationService.createDataSet(DatasetConverter.convertUserModelToDomainInformationModel(dataset));
+    }
 
-	@Override
-	public void updateDataset(DatasetExtension dataset) throws UserServiceException {
-		informationService.updateDataSet(DatasetConverter.convertUserModelToDomainInformationModel(dataset));
-	}
+    @Override
+    public void deleteDataset(DatasetExtension dataset) {
+        informationService.deleteDataSet(DatasetConverter.convertUserModelToDomainInformationModel(dataset));
+    }
 
-	@Override
-	public DatasetList findDataset(DatasetFilter datasetFilter) throws UserServiceException {
-		return DatasetConverter.convertInformationModelToUserModel(informationService.getDataSets(DatasetConverter.convertUserModelToDomainInformationModel(datasetFilter)));
-	}
-    
-	
+    @Override
+    public void updateDataset(DatasetExtension dataset) {
+        informationService.updateDataSet(DatasetConverter.convertUserModelToDomainInformationModel(dataset));
+    }
+
+    @Override
+    public DatasetList findDataset(DatasetFilter datasetFilter) {
+        return DatasetConverter.convertInformationModelToUserModel(informationService.getDataSets(DatasetConverter.convertUserModelToDomainInformationModel(datasetFilter)));
+    }
+
+
 }
