38aedb23d84c084bed45e0f4a4e23d339aa11ea3
lsotiriadis <leontion.sotiriadis@sword-group.com>
2020-07-29 17:59:13 +0300

    [UNIONVMS-4739] JAXBMarshaller refactoring
    Signed-off-by: lsotiriadis <leontion.sotiriadis@sword-group.com>

--- model/src/main/java/eu/europa/ec/fisheries/uvms/user/model/mapper/JAXBMarshaller.java
+++ model/src/main/java/eu/europa/ec/fisheries/uvms/user/model/mapper/JAXBMarshaller.java
@@ -1,32 +1,35 @@
 /*
- * Developed by the European Commission - Directorate General for Maritime 
+ * Developed by the European Commission - Directorate General for Maritime
  * Affairs and Fisheries © European Union, 2015-2016.
- * 
+ *
  * This file is part of the Integrated Fisheries Data Management (IFDM) Suite.
  * The IFDM Suite is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
- * The IFDM Suite is distributed in the hope that it will be useful, but 
+ * The IFDM Suite is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details. You should have received a copy of the GNU General Public 
+ * more details. You should have received a copy of the GNU General Public
  * License along with the IFDM Suite. If not, see http://www.gnu.org/licenses/.
  */
 package eu.europa.ec.fisheries.uvms.user.model.mapper;
 
+import eu.europa.ec.fisheries.uvms.commons.xml.AbstractJAXBMarshaller;
+import eu.europa.ec.fisheries.uvms.commons.xml.JAXBRuntimeException;
 import eu.europa.ec.fisheries.uvms.user.model.exception.ModelMarshallException;
-import java.io.StringReader;
-import java.io.StringWriter;
+
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JAXBMarshaller {
+import java.util.HashMap;
+import java.util.Map;
+
+public class JAXBMarshaller extends AbstractJAXBMarshaller {
 
     final static Logger LOG = LoggerFactory.getLogger(JAXBMarshaller.class);
 
@@ -36,18 +39,14 @@ public class JAXBMarshaller {
      * @param <T>
      * @param data
      * @return
-     * @throws
-     * eu.europa.ec.fisheries.uvms.user.model.exception.ModelMarshallException
+     * @throws eu.europa.ec.fisheries.uvms.user.model.exception.ModelMarshallException
      */
     public static <T> String marshallJaxBObjectToString(T data) throws ModelMarshallException {
         try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(data.getClass());
-            Marshaller marshaller = jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            StringWriter sw = new StringWriter();
-            marshaller.marshal(data, sw);
-            return sw.toString();
-        } catch (JAXBException ex) {
+            Map<String,Object> properties = new HashMap<>();
+            properties.put(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            return marshallToString(data, properties);
+        } catch (JAXBException | JAXBRuntimeException ex) {
             throw new ModelMarshallException("[ Error when marshalling Object to String ]", ex);
         }
     }
@@ -58,20 +57,15 @@ public class JAXBMarshaller {
      *
      * @param <R>
      * @param textMessage
-     * @param clazz pperException
+     * @param clazz       pperException
      * @return
-     * @throws
-     * eu.europa.ec.fisheries.uvms.user.model.exception.ModelMarshallException
+     * @throws eu.europa.ec.fisheries.uvms.user.model.exception.ModelMarshallException
      */
-    public static <R> R unmarshallTextMessage(TextMessage textMessage, Class clazz) throws ModelMarshallException {
+    public static <R> R unmarshallTextMessage(TextMessage textMessage, Class<R> clazz) throws ModelMarshallException {
         try {
-            JAXBContext jc = JAXBContext.newInstance(clazz);
-            Unmarshaller unmarshaller = jc.createUnmarshaller();
-            StringReader sr = new StringReader(textMessage.getText());
-            return (R) unmarshaller.unmarshal(sr);
-        } catch (JMSException | JAXBException ex) {
+            return unmarshallTo(textMessage, clazz);
+        } catch (JMSException | JAXBException | JAXBRuntimeException ex) {
             throw new ModelMarshallException("[Error when unmarshalling response in ResponseMapper ]", ex);
         }
     }
-
 }
